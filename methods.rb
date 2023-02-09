2b1bcb13ce3e646b20137a26073d9c715551211b
Preston <prestonosoro56@gmail.com>
2023-02-09 16:26:08 +0300

    feat:write methods

--- methods.rb
+++ methods.rb
@@ -1,1 +1,30 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+greet_programmer
+
+def greet name
+    puts "Hello, #{name}!"
+end
+greet "Naureen"
+
+def greet_with_default name = "programmer"
+    puts "Hello, #{name}!"
+end
+greet_with_default "Naureen"
+greet_with_default 
+
+def add num1, num2
+    num1 + num2
+end
+sum = add 1, 2
+puts sum
+
+def halve number
+    if number.class != Integer
+        return nil
+    end
+    number/2
+end
+
